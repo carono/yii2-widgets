8758db6e3899f66f58a1e5453d3e100cf5d2e4fa
carono <info@carono.ru>
2017-04-16 18:27:38 +0500

    fix

--- Asset.php
+++ Asset.php
@@ -8,6 +8,6 @@ use yii\web\AssetBundle;
 
 class Asset extends AssetBundle
 {
-    public $sourcePath = '@carono/yii2-widgets/assets';
+    public $sourcePath = '@vendor/carono/yii2-widgets/assets';
     public $js = ['phones.js'];
 }
